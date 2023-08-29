d006d6646d214010adae7e840e32f5d66a3b16ef
GRFViper <1480812228@qq.com>
2023-08-29 15:02:25 +0800

    界面优化1.5

--- app/src/main/java/com/example/aclass/teacher/response/vo/CheckDetailAdapter.java
+++ app/src/main/java/com/example/aclass/teacher/response/vo/CheckDetailAdapter.java
@@ -13,6 +13,8 @@ import com.example.aclass.R;
 import com.example.aclass.teacher.response.GetCheckDetailResponse;
 import org.jetbrains.annotations.NotNull;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class CheckDetailAdapter extends RecyclerView.Adapter<CheckDetailAdapter.ViewHolder>{
@@ -33,19 +35,34 @@ public class CheckDetailAdapter extends RecyclerView.Adapter<CheckDetailAdapter.
     @Override
     public void onBindViewHolder(@NonNull CheckDetailAdapter.ViewHolder holder, int position) {
         GetCheckDetailResponse.Data dataItem = data.get(position);
-        holder.stuId.setText(dataItem.getUserId());
+        holder.stuId.setText(holder.stuId.getText()+dataItem.getUserId());
         holder.checkCourse.setText(dataItem.getCourseName());
         holder.addrCourse.setText(dataItem.getCourseAddr());
 //        holder.checkStatus.setText(dataItem.getStatus());
+        long timestamp = Long.parseLong(dataItem.getSignTime());
+        // 创建一个SimpleDateFormat对象，定义日期格式
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date date;
+        String dateString;
         if(dataItem.getStatus()==0)
         {
             holder.checkStatus.setText("未签到");
+            holder.checkTime.setText("");
         }
         else {
             holder.checkStatus.setText("已签到");
+            // 将时间戳转换为Date对象
+            date = new Date(timestamp);
+            // 使用SimpleDateFormat将Date对象格式化为日期字符串
+            dateString = sdf.format(date);
+            System.out.println("签到时间：" + dateString);
+            holder.checkTime.setText(holder.checkTime.getText()+dateString);
         }
-        holder.checkTime.setText(dataItem.getSignTime());
-        holder.createTime.setText(dataItem.getCreateTime());
+        timestamp = Long.parseLong(dataItem.getSignTime());
+        date = new Date(timestamp);
+        dateString = sdf.format(date);
+        System.out.println("发起时间：" + dateString);
+        holder.createTime.setText(holder.createTime.getText()+dateString);
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
